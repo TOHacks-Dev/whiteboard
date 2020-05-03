2974fdab0b55af790cf5440e0f7b44b88d6d87e8
ao-shen <ao.shen@gmail.com>
2020-05-03 03:29:48 -0400

    added file upload

--- public/app.js
+++ public/app.js
@@ -777,3 +777,29 @@ function changeStroke() {
     output.innerHTML = val;
     ctx.font = `${val*4 + 10}px Comic Sans MS`;
 }
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+function openModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
